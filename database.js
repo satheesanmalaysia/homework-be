8066115b262b431eed73b029b3e2d0c90d563744
LAPTOP-H1M1I4KI\Satheesan <satheesanmalaysia@gmail.com>
2024-05-28 21:00:15 +0800

    Adding database

---
+++ database.js
@@ -0,0 +1,25 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database.sqlite'); // Path to the database file
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS Homework (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        subject TEXT NOT NULL,
+        invited_date TEXT NOT NULL,
+        due_date TEXT NOT NULL,
+        num_of_submissions INTEGER,
+        homework_document TEXT
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS Student (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        invited_date TEXT NOT NULL,
+        status TEXT,
+        download_url TEXT
+    )`);
+});
+
+module.exports = db;
