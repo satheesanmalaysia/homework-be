ec92569f29fdbe5ddcae12e220b9edd08cb467a8
LAPTOP-H1M1I4KI\Satheesan <satheesanmalaysia@gmail.com>
2024-06-01 03:03:20 +0800

    Login service fixes

--- routes/users.js
+++ routes/users.js
@@ -34,7 +34,8 @@ router.post('/login', (req, res) => {
       }
       if (user && await bcrypt.compare(password, user.password)) {
           const accessToken = jwt.sign({ username: user.username, role: user.role, country: user.country }, SECRET_KEY, { expiresIn: '1h' });
-          res.json({ user,accessToken  });
+         
+          res.json({ accessToken, username: user.username ,role: user.role ,country: user.country });
       } else {
           res.status(401).send('Username or password incorrect');
       }
